8aa4b984675d03d7acc00f5d804d7a6e231fd12a
FolD WeAth <cheetah77@qq.com>
2017-06-23 20:11:28 +0800

    Client connection test

---
+++ Assets/Client/Script/ClientConnectionTest.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using UnityEngine;
+using System.Threading;
+
+public class ClientConnectionTest : MonoBehaviour {
+	Thread thread;
+	UdpClient udpSend, udpReceive;
+
+	void Start () {
+		udpSend = new UdpClient();
+		udpReceive = new UdpClient();
+		
+		thread = new Thread(new ThreadStart(ThreadFunc));
+		thread.Start();
+	}
+	
+	void OnApplicationQuit() {
+		thread.Abort();
+		udpSend.Close();
+		udpReceive.Close();
+	}
+
+	void ThreadFunc() {
+		while (true) {
+			var ep = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 12345);
+			udpSend.Connect(ep);
+			udpSend.Send(new byte[] { 1, 2, 3, 4, 5, 4, 3, 2, 1 }, 9);
+			print("Send data");
+			
+			var receivedData = udpSend.Receive(ref ep);
+
+			string str = "Received from " + ep + ", ";
+			foreach (var b in receivedData) {
+				str += b.ToString() + ",";
+			}
+
+			Debug.Log(str);
+
+			Thread.Sleep(500);
+		}
+		
+	}
+}
